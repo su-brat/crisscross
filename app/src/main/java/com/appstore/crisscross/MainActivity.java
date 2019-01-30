1956f19d9c3776ade27ffd29296c165ce08f55b8
Subrat Patnaik <subratpatnaik.email@gmail.com>
2019-01-30 18:47:15 +0530

    Bug fixed

--- app/src/main/java/com/appstore/crisscross/MainActivity.java
+++ app/src/main/java/com/appstore/crisscross/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -96,6 +97,7 @@ public class MainActivity extends AppCompatActivity {
                         finish();
                     }
                 });
+                gameActive = false;
                 builder.show();
                 return;
             } else {
